81e4215dfba8c0ffe02dc1063a7aa2e811a24cd8
flash <k.kostov.eu@gmail.com>
2017-10-09 13:24:49 +0300

    base app scaffold

---
+++ Flash.BitMarket/Flash.BitMarket.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Flash.BitMarket.Web.MvcApplication" Language="C#" %>
---
+++ Flash.BitMarket/Flash.BitMarket.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Flash.BitMarket.Web.Startup))]
+namespace Flash.BitMarket.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
