0509904bb8fe20756916a24a41553dd2cfcb7383
flash <k.kostov.eu@gmail.com>
2017-10-09 13:32:40 +0300

    added testing scaffold

---
+++ Flash.BitMarket/Flash.BitMarket.Tests/TestingTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flash.BitMarket.Tests
+{
+    [TestFixture]
+    public class TestingTests
+    {
+        [Test]
+        public void TestingTestFunctionality()
+        {
+            var a = 42;
+            var b = 72;
+
+            Assert.AreNotEqual(a, b);
+        }
+    }
+}
